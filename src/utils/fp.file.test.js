ec2652cb6246c30f757d752471ff1727cfcb02a2
ryuken <ryuken73@gmail.com>
2021-06-08 14:56:52 +0900

    etc: no change (inspect whether ELK saved bytes value is inaccurate. it shows accumulated saved size, not whole direcotry size)

--- src/utils/fp.file.test.js
+++ src/utils/fp.file.test.js
@@ -1,8 +1,19 @@
-const utils = require('./index_commonjs.js');
-const {getDirListR} = utils.file;
-const main = async dir => {
-    const list = await getDirListR(dir);
-    console.log(list)
+const {
+    getFileCountR,
+    getDirectoryCountR,
+    getTotalSizeR,
+    getDirListR,
+    getFileListR
+} = require('./fp.file_commonjs');
+
+const TEST_DIR = 'd:/temp/cctv';
+const main = async (dir) => {
+    console.log(await getFileCountR(dir));
+    console.log(await getDirectoryCountR(dir));
+    console.log(await getTotalSizeR(dir));
+    console.log(await getDirListR(dir));
+    console.log(await getFileListR(dir));
 }
 
-main('d:/temp/cctv')
+main(TEST_DIR)
+
